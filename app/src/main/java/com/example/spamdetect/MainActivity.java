e3d9f7aabb81b0f4e980f855691d2095b8cf0354
Yash Jagtap <yash.jagtap@spit.ac.in>
2023-08-09 14:46:53 +0530

    Pushed the code

---
+++ app/src/main/java/com/example/spamdetect/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.spamdetect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},1);
+        }
+    }
+}
